227d1151413ceb3550bf9fc4c11c54d90068eb09
johnyildr <71447139+johnyildr@users.noreply.github.com>
2020-09-25 14:42:09 -0400

    List questions

---
+++ questionslist.py
@@ -0,0 +1,14 @@
+# 1-) By typing spam['a', 'b', 'hello', 'd']
+# 2-) It evaluates to 0, assigning 0 as a variable.
+# 3-) It evaluates to assign -1 as an item inside spam.
+# 4-) It evaluates to assign -6 as an item inside spam.
+# 5-) It evaluates to assign :2 as an item inside spam.
+# 6-) bacon.index('cat') evaluates to 1
+# 7-) bacon.append(99) evaluates to add 99 to the list.
+# 8-) bacon.remove('cat') removes 'cat' from the list.
+# 9-) The operator for concatenation is +, while the operator for replication is *.
+# 10-) The difference between append and insert is that append can add new items to  the list but insert can modify an occupied position.
+# 11-) The first way is  using the remove() method and the other way is to use pop()
+# 12-) One of the similarities between string and list are that string is that they both can display things on the screen, secondly they both need print to display, and lastly they both
+# 13-) They contain items.
+# 14-) deepcopy() creates a new object which is a copy, while copy() only copies it to another object.
